f76c5ae198b3d96b7ca473032cedd6dfdc10239b
Shajidjamil <139593292+Shajidjamil@users.noreply.github.com>
2025-05-13 02:02:54 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+Num1 = int(input("Enter the first number :"))
+Num2 = int(input("Enter Second Number :"))
+print("\n")
+Addition = Num1 + Num2
+print("Addition:",Addition)
+Subtraction = Num1 - Num2
+print("Subtraction:",Subtraction)
+Multiplication = Num1 * Num2
+print("Multiplication:",Multiplication)
+Division = Num1 / Num2
+print("Division:",Division)
---
+++ Task2.py
@@ -0,0 +1,8 @@
+Num_one = input("Enter your first name:")
+Num_two= input("Enter your second name:")
+print("\n")
+print(f"Hello,{Num_one} {Num_two}! Welcome to the Python Program")
+
+
+
+
